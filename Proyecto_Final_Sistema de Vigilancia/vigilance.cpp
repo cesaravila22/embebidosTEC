1216050527c8926f3628f2fdfa59fc3460ea3d1c
Cesar <cesaravila22@gmail.com>
2013-11-15 13:06:13 -0600

    Version final del programa, proyecto final

--- Proyecto_Final_Sistema de Vigilancia/vigilance.cpp
+++ Proyecto_Final_Sistema de Vigilancia/vigilance.cpp
@@ -8,23 +8,26 @@
 using namespace std;
 using namespace cv;
 
-int reproducir_alarma(GstElement *pipeline){
+/* Function used to start the pipeline */
+int play_alarm(GstElement *pipeline){
     gst_element_set_state (pipeline, GST_STATE_PLAYING);
     return 0;
 }
 
-int detener_alarma(GstElement *pipeline){
+/* Function used to stop the pipeline */
+int stop_alarm(GstElement *pipeline){
     gst_element_set_state (pipeline, GST_STATE_NULL);
     return 0;
 }
 
+/* Main function of the program */
 int main (int argc, char * const argv[]){
 
+	/* Initialize the pipeline with all its arguments */
 	GstElement *pipeline, *source, *madmp3, *sink;
     GstBus *bus;
     guint bus_watch_id;
 
-    /* Initialisation */
     gst_init (NULL, NULL);
 
     /* Create gstreamer elements */
@@ -39,39 +42,46 @@ int main (int argc, char * const argv[]){
     }
 
     /* Set up the pipeline */
-    /* we set the input filename to the source element */
+    /* Set the input filename to the source element */
     g_object_set (G_OBJECT (source), "location", "Sirena-Policia.mp3", NULL);
-    /* we add all elements into the pipeline */
+
+    /* Add all elements into the pipeline */
     gst_bin_add_many (GST_BIN (pipeline), source, madmp3, sink, NULL);
-    /* we link the elements together */
+
+    /* Link the elements together */
     gst_element_link_many (source, madmp3, sink, NULL);
 	
-    const int DELAY = 100; // mseconds
+	/* Delay in ms */
+    const int DELAY = 100;
 	
+	/* Variable used to check alarm on or off */	
 	bool alarm_activated = false;
     
-    // create all necessary instances
+    /* Initialize all necesary instances for opencv */
+	/* Get capture from camera */
     CvCapture * camera = cvCaptureFromCAM(1);
+	/* Initialize 2 matrixes for comparition purposes */
     Mat next_frame_color = cvQueryFrame(camera);
     Mat current_frame_color = cvQueryFrame(camera);
 	
     Mat current_frame, next_frame;
     
+	/* Convert the matrixes from color to gray scale */
     cvtColor(current_frame_color, current_frame, CV_RGB2GRAY);
     cvtColor(next_frame_color, next_frame, CV_RGB2GRAY);  
 	
+	/* Initialize the window */
     imshow("Motion1", current_frame);	  
 
+	/* Initialize the loop to start detecting movement */
     Mat d1, d2, result;
     int window = 200;
     bool movement;
     while (true)
-    {
-        //imshow("Motion1", current_frame);
-        //imshow("Motion2", next_frame);
-        
+    {        
         movement = false;
         imshow("Motion1", next_frame);
+		/* Get difference between matrixes */
         absdiff(next_frame, current_frame, d1);  	
         absdiff(current_frame, current_frame, d2);
         bitwise_xor(d1, d2, result);
@@ -79,7 +89,7 @@ int main (int argc, char * const argv[]){
         int middle_y = result.rows/2;
         int middle_x = result.cols/2;
         
-        // Center window
+        /* Center window */
         threshold(result, result, 140, 255, CV_THRESH_BINARY);
         for(int i = middle_x-window; i < middle_x+window; i++)
             for(int j = middle_y-window; j < middle_y+window; j++)
@@ -89,34 +99,31 @@ int main (int argc, char * const argv[]){
                     break;
                 }
         
+		/* Action taken when movement */
         if(movement==true){
 			if (alarm_activated){
 				cout<<"Movement!\n";
 			} else{
 				alarm_activated = true;
 				cout<<"Movement!\n";
-				reproducir_alarma(pipeline);
+				play_alarm(pipeline);
 			}
         }
 
-        //imshow("Motion", result);
-        
+		/* Copy the future matrix in the current matrix */        
         next_frame.copyTo(current_frame);
 
-        // semi delay and quit when press Q/q
+        /* Delay to wait for action: quit program when press Q/q, stop alarm when press S/s */
         int key = cvWaitKey (DELAY);
         if (key == 'q' || key == 'Q'){
             break;
 		}
 		if (key == 's' || key == 'S'){
-            detener_alarma(pipeline);
+            stop_alarm(pipeline);
 			alarm_activated = false;
 		}
-
-        //cvReleaseCapture(&camera);
-        //camera = cvCaptureFromCAM(0);
         
-        // get image from webcam
+        /* Get new image from webcam for comparison */
         next_frame_color = cvQueryFrame(camera);
         cvtColor(next_frame_color, next_frame, CV_RGB2GRAY);
     }
